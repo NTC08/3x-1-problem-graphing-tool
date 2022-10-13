6e850809f825fde71b8a6ec7dfe5ebf5775a9dde
no no <78420855+NTC08@users.noreply.github.com>
2022-10-13 17:18:27 +0700

    Add files via upload

--- 3x+1.py
+++ 3x+1.py
@@ -1,7 +1,22 @@
 import pygame
 import sys
 
-number = int(input("input number : "))
+q = 0
+color = ["red", "green", "beige", "blue", "brown", "pink", "darkviolet", "gray", "navy", "moccasin", "orange", "yellow"]
+color2 = 0
+
+w = 0
+w = input("how many inputs : ")
+w = int(w)
+
+number2 = []
+while len(number2) < w:
+    number2.append(input("input number : "))
+    print(number2)
+
+number = number2[q]
+number = int(number)
+
 screen_size_x = (input("input screen size x : "))
 if bool(screen_size_x) == False:
     screen_size_x = 800
@@ -19,7 +34,7 @@ scale_y = float(scale_y)
 
 space = input("input space between points : ")
 if bool(space) == False:
-    space = 2
+    space = 5
 space = float(space)
 
 width = input("input width : ")
@@ -70,23 +85,35 @@ while True:
             steps = steps + 1
             print(number)
     if ss != 0:
-        pygame.draw.line(screen, "red", ((old_pos_x), ((old_pos_y*scale_y)-screen_size_y)*-1), ((new_pos_x), ((new_pos_y*scale_y)-screen_size_y)*-1), width)
-        text = font.render("input = " + str(input_number) + " ", False, "white")
+        pygame.draw.line(screen, color[color2], ((old_pos_x), ((old_pos_y*scale_y)-screen_size_y)*-1), ((new_pos_x), ((new_pos_y*scale_y)-screen_size_y)*-1), width)
+        text = font.render("current input = " + str(input_number) + "   ", False, "white")
         text_rec = text.get_rect(topleft = (10,10))
         pygame.draw.rect(screen, "black",  text_rec,)
         screen.blit(text,(10,10))
         
-        steps_text = font.render("current step = " + str(steps) + " ", False, "white")
+        steps_text = font.render("current step = " + str(steps) + "   ", False, "white")
         step_rec = steps_text.get_rect(topleft = (10,30))
         pygame.draw.rect(screen, "black",  step_rec,)
         screen.blit(steps_text,(step_rec))
         
-        number_text = font.render("current number = " + str(number) + "           ", False, "white")
+        number_text = font.render("current number = " + str(number) + "    ", False, "white")
         number_rec = number_text.get_rect(topleft = (10,50))
         pygame.draw.rect(screen, "black",  number_rec,)
         screen.blit(number_text,(number_rec))
         
         pygame.display.update()
         Clock.tick(speed)
+    
     if number == 1.0:
-        ss = 0
+        if q + 1 < len(number2): 
+            q = q + 1
+            number = int(number2[q])
+            new_pos_x = 0
+            new_pos_y = number
+            input_number = number
+            color2 = q 
+            if color2 + 1 > len(color):
+                color2 = color2 - len(color)
+            steps = 0
+        else:
+            ss = 0
